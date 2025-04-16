e07021ec89a4e0db869062c541384e9fd253f2df
HeRa Khan <heraemt@gmail.com>
2025-04-16 14:46:35 +0600

    1

---
+++ plugins/text.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// কম্পোনেন্ট ১: হেডার ডিজাইন এ
+export const hello = () => (
+  <div className="bg-blue-100 text-white p-4">
+    <h1 className="text-2xl font-bold">HeLLo</h1>
+  </div>
+);
+// একশন কনফিগারেশন
+export const actions = [
+    { hookName: 'Nex-header', position: 0, componentName: 'hello' },
+];
